8527d2c478e17dffde5b3f0de81e3695b34e3b34
Victor Axelrod <victora@marvell.com>
2016-01-17 14:11:10 +0200

    arm: mvebu: a38x: ddr: Fix ADLL calibration config
    This patch fixes ADLL calibration configuration by:
    - VALIDATE_BUS_ACTIVE macro removal
      (CTRL SUBPHY are always active) and
    - proper set of CTRL SUBPHY numbers.
    
    Change-Id: I704024a74f4a162d19ac40b160046d5b983d7885
    Signed-off-by: Victor Axelrod <victora@marvell.com>
    Reviewed-on: http://vgitil04.il.marvell.com:8080/26843
    Reviewed-by: Nadav Haklai <nadavh@marvell.com>
    Tested-by: Nadav Haklai <nadavh@marvell.com>

--- ddr3_training.c
+++ ddr3_training.c
@@ -1538,8 +1538,7 @@ int adll_calibration(u32 dev_num, enum hws_access_type access_type,
 			      freq_config_info.rate_per_freq, 0x7));
 	}
 
-	for (bus_cnt = 0; bus_cnt < octets_per_if_num; bus_cnt++) {
-		VALIDATE_BUS_ACTIVE(tm->bus_act_mask, bus_cnt);
+	for (bus_cnt = 0; bus_cnt < DDR_IF_CTRL_SUBPHYS_NUM; bus_cnt++) {
 		CHECK_STATUS(ddr3_tip_bus_read_modify_write
 			     (dev_num, ACCESS_TYPE_UNICAST, if_id, bus_cnt,
 			      DDR_PHY_CONTROL, BW_PHY_REG,
