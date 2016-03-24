7d8b1fbb4c7932499551656e093fcd7f21f318f0
Ofir Fedida <ofedida@marvell.com>
2016-03-24 12:27:20 +0200

    mv_ddr: Add read modify write function to atf wrapper
    Change-Id: I3163c9c505b3ebc17605f55b70839e98515c720b
    Reviewed-on: http://vgitil04.il.marvell.com:8080/28497
    Reviewed-by: Nadav Haklai <nadavh@marvell.com>
    Tested-by: Nadav Haklai <nadavh@marvell.com>

--- mv_ddr_atf_wrapper.h
+++ mv_ddr_atf_wrapper.h
@@ -268,8 +268,14 @@ static inline void mmio_write2_32(u32 val, u32 addr)
 	mmio_write_32(addr, val);
 }
 
+static inline void mmio_clrsetbits2_32(u32 addr, u32 val, u32 mask)
+{
+	mmio_clrsetbits2_32(addr, mask, val);
+}
+
 #define writel mmio_write2_32
 #define readl mmio_read_32
+#define writel_mask mmio_clrsetbits2_32
 
 #if defined(CONFIG_DDR4)
 #define vref_calibration_wa vrefCalibrationWA
