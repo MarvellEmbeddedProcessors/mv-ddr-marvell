e93462e92e37901a2ba7a87c723b99c7c710bceb
Victor Axelrod <victora@marvell.com>
2018-01-04 15:29:34 +0200

    mv_ddr: fix header files reference in ddr_training_ip_db.h
    Change-Id: Ib5686e805c866b76d1d5d1e0fd934c494369d8a9
    Signed-off-by: Victor Axelrod <victora@marvell.com>
    Reviewed-on: http://vgitil04.il.marvell.com:8080/48539
    Reviewed-by: Ofir Fedida <ofedida@marvell.com>
    Tested-by: iSoC Platform CI <ykjenk@marvell.com>

--- ddr_training_ip_db.h
+++ ddr_training_ip_db.h
@@ -98,7 +98,6 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #ifndef _DDR_TRAINING_IP_DB_H_
 #define _DDR_TRAINING_IP_DB_H_
 
-#include "ddr_topology_def.h"
 #include "ddr3_training_ip_db.h"
 
 u32 pattern_table_get_word(u32 dev_num, enum hws_pattern type, u8 index);
