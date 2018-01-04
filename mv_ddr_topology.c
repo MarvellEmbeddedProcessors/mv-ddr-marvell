7de3e14497a150edf0b25ffc5d5bb134cc5cc692
Victor Axelrod <victora@marvell.com>
2018-01-04 15:57:23 +0200

    mv_ddr: fix header files reference in mv_ddr_topology.c
    Change-Id: I1affc0b9975ce86d30bb89e1c5094207dcb8fd35
    Signed-off-by: Victor Axelrod <victora@marvell.com>
    Reviewed-on: http://vgitil04.il.marvell.com:8080/48542
    Reviewed-by: Ofir Fedida <ofedida@marvell.com>
    Tested-by: iSoC Platform CI <ykjenk@marvell.com>

--- mv_ddr_topology.c
+++ mv_ddr_topology.c
@@ -95,10 +95,27 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 *******************************************************************************/
 
+#if defined(MV_DDR) /* U-BOOT MARVELL 2013.01 */
+#include "ddr_mv_wrapper.h"
+#elif defined(MV_DDR_ATF) /* MARVELL ATF */
+#include "mv_ddr_atf_wrapper.h"
+#elif defined(CONFIG_A3700)
+#include "mv_ddr_a3700_wrapper.h"
+#else /* U-BOOT SPL */
+#include "ddr_ml_wrapper.h"
+#endif
+
+#if defined(CONFIG_ARMADA_38X) || defined(CONFIG_ARMADA_39X)
+#include "a38x/mv_ddr_plat.h"
+#elif defined(CONFIG_APN806)
+#include "apn806/mv_ddr_plat.h"
+#elif defined(CONFIG_A3700)
+#include "a3700/mv_ddr_plat.h"
+#endif
+
 #include "mv_ddr_topology.h"
 #include "mv_ddr_common.h"
 #include "mv_ddr_spd.h"
-#include "ddr3_init.h"
 #include "ddr_topology_def.h"
 #include "ddr3_training_ip_db.h"
 #include "ddr3_training_ip.h"
