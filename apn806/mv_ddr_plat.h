c6d180d5384c7831b48e92134cfa618c4e62e401
Victor Axelrod <victora@marvell.com>
2018-01-25 15:34:19 +0200

    mv_ddr: apn806: fix platform header's guard macro
    Change-Id: I419b2be2c1509feb0b162dc162b30f677b438d47
    Signed-off-by: Victor Axelrod <victora@marvell.com>
    Reviewed-on: http://vgitil04.il.marvell.com:8080/49578
    Tested-by: iSoC Platform CI <ykjenk@marvell.com>

--- apn806/mv_ddr_plat.h
+++ apn806/mv_ddr_plat.h
@@ -95,8 +95,8 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 *******************************************************************************/
 
-#ifndef _MV_DDR_AP806_H
-#define _MV_DDR_AP806_H
+#ifndef _MV_DDR_PLAT_H
+#define _MV_DDR_PLAT_H
 
 #include "mv_ddr_static.h"
 
@@ -398,4 +398,4 @@ int mv_ddr_post_training_fixup(void);
 int mv_ddr_manual_cal_do(void);
 int mv_ddr_electrical_data_set(unsigned int edata[], unsigned int cs_num);
 
-#endif /* _MV_DDR_AP806_H */
+#endif /* _MV_DDR_PLAT_H */
