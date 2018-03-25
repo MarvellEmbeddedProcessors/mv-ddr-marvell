81eb48dff468dc72d6adfddc260cf1863406d5de
Ofir Fedida <ofedida@marvell.com>
2018-03-25 17:21:18 +0300

    mv_ddr: drivers: snps: add workaround for apn807 x16 devices support
    Adds APN807 workaround for x16 devices.
    In PDA mode, VREF is set per PHY. There are two PHYs per x16 device
    with possible different VREF values. The workaround overrides x16 to
    x8 device configuration to ensure the first connected PHY's VREF value
    is taken.
    
    Change-Id: I727d3656e241692ddf98eb22e061e0ec7507a9c0
    Signed-off-by: Ofir Fedida <ofedida@marvell.com>
    Reviewed-on: http://vgitil04.il.marvell.com:8080/52500
    Tested-by: iSoC Platform CI <ykjenk@marvell.com>
    Reviewed-by: Victor Axelrod <victora@marvell.com>

--- drivers/snps/snps_update.c
+++ drivers/snps/snps_update.c
@@ -593,7 +593,18 @@ u16 dmem_1d_2d_gear_down_x16_present_get(void)
 
 	x16_present_val = (iface_params->bus_width == MV_DDR_DEV_WIDTH_8BIT) ?
 			   X16_NOT_PRESENT_VAL :
+#ifdef APN807
+/*
+ * APN807 workaround for x16 devices
+ * In PDA mode, VREF is set per PHY.
+ * There are two PHYs per x16 device with possible different VREF values.
+ * The workaround overrides x16 to x8 device configuration to ensure
+ * the first connected PHY's VREF value is taken.
+ */
+			   X16_NOT_PRESENT_VAL;
+#else
 			   X16_PRESENT_VAL;
+#endif
 
 	if (cs_num == 1) /* single cs */
 		ret_val = (x16_present_val & X16_PRESENT_CS0_MASK) << X16_PRESENT_CS0_OFFS;
