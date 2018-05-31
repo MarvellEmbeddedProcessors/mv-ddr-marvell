d4677e099dcebe3a81bc7fe483437120bdfb768a
Konstantin Porotchkin <kostap@marvell.com>
2018-05-31 11:23:10 +0300

    mv_ddr: snps: Support GCC 5.3.1 and 7.2.1 with FW images
    Support GCC 5.3.1 and GCC 7.2.1 with SNPS FW images.
    
    Change-Id: Id8eeb5fcd44d87525f324c864e5e43c234419b8e
    Signed-off-by: Konstantin Porotchkin <kostap@marvell.com>
    Reviewed-on: http://vgitil04.il.marvell.com:8080/56061
    Tested-by: iSoC Platform CI <ykjenk@marvell.com>
    Reviewed-by: Victor Axelrod <victora@marvell.com>

--- drivers/snps/snps_ddr.c
+++ drivers/snps/snps_ddr.c
@@ -95,7 +95,13 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 *******************************************************************************/
 
-#include "snps_static.h"
+#if (__GNUC__ == 5  && __GNUC_MINOR__ == 3 && __GNUC_PATCHLEVEL__ == 1)
+#include "snps_static-gcc-5.h"
+#elif (__GNUC__ == 7  && __GNUC_MINOR__ == 2 && __GNUC_PATCHLEVEL__ == 1)
+#include "snps_static-gcc-7.h"
+#else
+#error "Unsupported GCC version! Please rebuild your SNPS FW image."
+#endif
 #include "snps.h"
 #include "snps_update.h"
 #include "snps_regs.h"
