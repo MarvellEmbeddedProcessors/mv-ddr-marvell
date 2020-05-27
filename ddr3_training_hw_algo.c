81be5175a8b3fbb9f967a9de959d4d5ea523f25e
Chris Packham <chris.packham@alliedtelesis.co.nz>
2020-05-27 12:53:14 +1200

    mv_ddr: ddr3: Use correct bitmask for read sample delay
    In the Armada 385 functional spec (MV-S109094-00 Rev. C) the read sample
    delay fields are 5 bits wide. Use the correct bitmask of 0x1f when
    extracting the value.
    
    Signed-off-by: Chris Packham <chris.packham@alliedtelesis.co.nz>

--- ddr3_training_hw_algo.c
+++ ddr3_training_hw_algo.c
@@ -103,7 +103,7 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #define VREF_MAX_INDEX			7
 #define MAX_VALUE			(1024 - 1)
 #define MIN_VALUE			(-MAX_VALUE)
-#define GET_RD_SAMPLE_DELAY(data, cs)	((data >> rd_sample_mask[cs]) & 0xf)
+#define GET_RD_SAMPLE_DELAY(data, cs)	((data >> rd_sample_mask[cs]) & 0x1f)
 
 u32 ca_delay;
 int ddr3_tip_centr_skip_min_win_check = 0;
